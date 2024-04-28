e0d55600d4cc4e635393ee4de70eaf9d66990db6
rixiobarrios <rixiobarrios.consulado@gmail.com>
2024-04-28 13:36:53 -0500

    answers for lesson 10

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -3,11 +3,26 @@ const { StatusCodes } = require('http-status-codes');
 const { BadRequestError, NotFoundError } = require('../errors');
 
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs');
+    // res.send('get all jobs');
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort(
+        'createdAt'
+    );
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
 const getJob = async (req, res) => {
-    res.send('get job');
+    // res.send('get job');
+    // nested destructuring
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req;
+
+    const job = await Job.findOne({ _id: jobId, createdBy: userId });
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`);
+    }
+    res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
@@ -20,11 +35,38 @@ const createJob = async (req, res) => {
 };
 
 const updateJob = async (req, res) => {
-    res.send('update job');
+    // res.send('update job');
+    const {
+        body: { company, position },
+        user: { userId },
+        params: { id: jobId },
+    } = req;
+    if (company === '' || position === '') {
+        throw new BadRequestError('company and/or position cannot be empty');
+    }
+    const job = await Job.findByIdAndUpdate(
+        { _id: jobId, createdBy: userId },
+        req.body,
+        { new: true, runValidators: true }
+    );
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`);
+    }
+    res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-    res.send('delete job');
+    // res.send('delete job');
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req;
+
+    const job = await Job.findByIdAndRemove({ _id: jobId, createdBy: userId });
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`);
+    }
+    res.status(StatusCodes.OK).send();
 };
 
 module.exports = {
--- 06-jobs-api/starter/middleware/error-handler.js
+++ 06-jobs-api/starter/middleware/error-handler.js
@@ -1,10 +1,36 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+// const { CustomAPIError } = require('../errors');
+const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+    // console.log(err)
+    let customError = {
+        // set default
+        statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: err.message || 'something went wrong, try again later',
+    };
 
-module.exports = errorHandlerMiddleware
+    // if (err instanceof CustomAPIError) {
+    //     return res.status(err.statusCode).json({ msg: err.message });
+    // }
+    if (err.name === 'ValidationError') {
+        // console.log(Object.values(err.errors));
+        customError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(',');
+        customError.statusCode = 400;
+    }
+    if (err.code && err.code === 11000) {
+        // customError.msg = `duplicate value entered for ${err.keyValue} field`;
+        customError.msg = `duplicate value entered for ${Object.keys(
+            err.keyValue
+        )} field`;
+        customError.statusCode = 400;
+    }
+    if (err.name === 'CastError') {
+        customError.msg = `no item found with id: ${err.value}`;
+        customError.statusCode = 404;
+    }
+    // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
+    return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
